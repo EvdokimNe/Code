1f8b819fecc223d2c59f15b219a64bc1d3fbfb71
EvdokimNe <60344915+EvdokimNe@users.noreply.github.com>
2020-08-19 16:22:24 +0300

    Add files via upload

---
+++ GameCntrl.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
+
+public class GameCntrl : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _mainCamera;
+    [SerializeField]
+    private LayerMask _layerMask;
+    [SerializeField]
+    private Text _score;
+    [SerializeField]
+    private string _nextScene;
+
+    private List<People> _selectedObjects = new List<People>();
+    private List<People> _allObjects = new List<People>();
+    private List<GameObject> _coinsLevel = new List<GameObject>();
+    
+    private bool _canMove = true;
+    private bool _win = false;
+
+    private int _localCoins;
+    private int _coins;
+    
+    private void Start()
+    {
+        AddAllCoins();
+        var objects = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var gameObject in objects)
+        {
+           _allObjects.Add(gameObject.GetComponent<People>()); 
+        }
+        _coinsLevel = GameObject.FindGameObjectsWithTag("Coin").ToList();
+    }
+    
+    private IEnumerator CreateRoadCord(GameObject selectedObj)
+    {
+        var points = new List<Vector3>();
+        points.Add(selectedObj.transform.position);
+        People people = selectedObj.GetComponent<People>();
+        if (people._finished)
+        {
+            people.ResetFinished();
+            SelectedObjDell(people);
+            yield break;
+        }
+        SelectedObjAdd(people);
+
+        while (true)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit hit, 100f,~_layerMask);
+            var pointPosition = hit.point;
+            pointPosition.y = 0.02f;
+            if ((points[points.Count - 1] - pointPosition).magnitude > 0.05f)
+            {
+                points.Add(pointPosition);
+            }
+            if (Input.GetMouseButtonUp(0) || hit.collider == null)
+            {
+                people.RoadPoints(points);
+                SelectedObjStartMove();
+                yield break; 
+            }
+            people.OnDrawLine(points.ToArray());
+            yield return null;
+        }
+    }
+    
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit hit, 100f);
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Player"))
+            {
+                if (CanSelect())
+                {
+                    StartCoroutine(CreateRoadCord(hit.collider.gameObject));
+                }
+            }
+        }
+    }
+
+    private bool CanSelect()
+    {
+        for (int i = 0; i < _selectedObjects.Count; i++)
+        {
+            if (_selectedObjects[i]._moveLastPos == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void BackUp()
+    {
+        for (int i = 0; i < _allObjects.Count; i++)
+        {
+            if (_allObjects[i]._moveLastPos == false)
+            {
+                return;
+            }
+        }
+        CoinsBack();
+        for (int x = 0; x < _allObjects.Count; x++)
+        {
+            _allObjects[x].ParkedCheck();
+        }
+    }
+    public void CheckWin()
+    {
+        for (int i = 0; i < _allObjects.Count; i++)
+        {
+            if (_allObjects[i]._finished == false)
+            {
+                return;
+            }
+        }
+        _win = true;
+        AddAllCoins();
+        Invoke("LoadNextSceneMethod",2f);
+    }
+    
+    private void SelectedObjStartMove()
+    {
+        foreach (var obj in _selectedObjects)
+        {
+            obj.Moved();
+        }
+    }
+    private void SelectedObjAdd(People selectedObj)
+    {
+        if(!_selectedObjects.Contains(selectedObj))
+        {
+            _selectedObjects.Add(selectedObj);
+        }
+    }
+    private void SelectedObjDell(People selectedObj)
+    {
+        if(_selectedObjects.Contains(selectedObj))
+        {
+            _selectedObjects.Remove(selectedObj);
+        }
+    }
+
+    internal void AddLocalCoins()
+    {
+        _localCoins++;
+    }
+    private void CoinsBack()
+    {
+        _localCoins = 0;
+        if (!_win)
+        {
+            foreach (var coins in _coinsLevel)
+            {
+                coins.transform.position = new Vector3(coins.transform.position.x, 0, coins.transform.position.z);
+            }
+        }
+    }
+    private void AddAllCoins()
+    {
+        _coins += _localCoins;
+        var c = _coins.ToString();
+        _score.text = c;
+    }
+
+    private void LoadNextSceneMethod()
+    {
+        SceneManager.LoadScene(_nextScene.ToString());
+    }
+}
+
---
+++ People.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class People : MonoBehaviour
+{
+    [SerializeField] private GameCntrl _GK;
+    [SerializeField] private GameObject _finalPoint;
+    [GradientUsage(true)]
+    [SerializeField] private Gradient _gradient;
+    [SerializeField] private LineRenderer _line;
+    
+    [SerializeField] private float _speed;
+    [SerializeField] private Animator _animator;
+   
+    private Vector3 _startPosition;
+    private Quaternion _startRotation;
+
+    internal bool _finished;
+    public bool _moveLastPos = true;
+    
+    private List<Vector3> _road = new List<Vector3>();
+    private int _roadPoint = 0;
+
+
+    public void Start()
+    {
+        _moveLastPos = true;
+        _line.enabled = false;
+        _startPosition = transform.position;
+        _startRotation = transform.rotation;
+        _line.colorGradient = _gradient;
+    }
+
+    private IEnumerator MoveToRoad()
+    {
+        _moveLastPos = false;
+        _finished = false;
+        transform.position = _startPosition;
+        _animator.SetTrigger("Run");
+        while (true)
+        {
+            if (_animator.GetBool("Dead") == true)
+            {
+                yield break;
+            }
+            transform.LookAt(_road[_roadPoint]);
+            transform.position = Vector3.MoveTowards(transform.position, _road[_roadPoint], _speed * Time.deltaTime);
+            if (_roadPoint == _road.Count - 1)
+            {
+                _moveLastPos = true;
+                Invoke("BackUpThis",0.5f);
+                _animator.SetTrigger("Stop");
+                _roadPoint = 0;
+                yield break;
+            }
+
+            if (Vector3.Distance(_road[_roadPoint], transform.position) < 0.01f)
+            {
+                _roadPoint++;
+            }
+
+            yield return null;
+        }
+        
+    }
+
+    private void BackUpThis()
+    {
+        _animator.SetTrigger("Stop");
+        _GK.BackUp();
+    }  
+        
+    public void OnDrawLine(Vector3[] point)
+    {
+        _line.enabled = true;
+        _line.positionCount = point.Length;
+        _line.SetPositions(point);
+    }
+
+    public void RoadPoints(List<Vector3> point)
+    {
+        _road = point;
+    }
+
+    public void Moved()
+    {
+        StartCoroutine(MoveToRoad());
+    }
+
+    public void ParkedCheck()
+    {
+        _moveLastPos = true;
+        if (!_finished)
+        {
+            transform.position = _startPosition;
+            transform.rotation = _startRotation;
+        }
+    }
+
+    public void ResetFinished()
+    {
+        transform.position = _startPosition;
+        transform.rotation = _startRotation;
+        _line.enabled = false;
+        _finished = false; 
+        _moveLastPos = true;
+        _animator.SetTrigger("Stop");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _finished = false;
+            _moveLastPos = true;
+            collision.gameObject.GetComponent<People>()._moveLastPos = true;
+            _animator.SetBool("Dead",true);
+            _roadPoint = 0;
+            Invoke("BackUpThis", 3.5f);
+        }
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Coin"))
+        {
+            _GK.AddLocalCoins();
+            coll.gameObject.transform.position = new Vector3(coll.gameObject.transform.position.x,100,coll.gameObject.transform.position.z);
+        }
+    } 
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject == _finalPoint && _moveLastPos && !_finished)
+        {
+            _animator.SetTrigger("Finish");
+            _finished = true;
+            _GK.CheckWin();
+        }
+    } 
+}
+
